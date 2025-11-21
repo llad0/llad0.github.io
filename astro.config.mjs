afe30be3774ae7e4a1724246e54dfa6904e7f58e
llad0 <joanllado123@gmail.com>
2025-11-21 12:49:50 +0100

    Downgrade tailwind to v3 and add math support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://llad0.github.io', 
+  site: 'https://llad0.github.io',
   integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
